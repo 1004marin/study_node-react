4bdba0ddba7138b09ad7b20f0c1dcb404cc949a6
Jo yerin <yerin65@naver.com>
2024-01-03 23:54:48 +0900

    비번 암호화

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')//express모듈 가져오기
+const express = require('express')
 const app = express()//새로운 express앱만들기
-const port = 9000//port는 내맘대로. 백서버로 두는 포트임
+const port = 9000
 
 const bodyParser = require('body-parser');
 const {User} = require("./model/User");
 
 
 
-//bodyparser 설정
+
 app.use(bodyParser.urlencoded({extended:true}))
 //application/x-www-form-urlencoded된 데이터를 분석할 수 있게
 
@@ -20,22 +20,18 @@ app.use(bodyParser.json());
 const mongoose= require('mongoose')
 const config = require('./config/key.js');
 
-mongoose.connect(config.mongoURI,{
-    //useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex: true, useFinedAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI)
+.then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(config, config.mongoURI))
-//use~안쓰면 에러 뜨기도
 
 
-app.get('/', (req, res) => res.send('녕안녕LD!'))//'/'루트 디렉토리에 오면 hello world가 출력되도록 함
+app.get('/', (req, res) => res.send('녕안녕LD!'))
 
 //엔드포인트 register
 app.post('/register', async(req,res) => {
-//회원가입할 때 필요한 정보를 client에서 가져오면
-//그것들을 db에 넣어준다.
-//=> user모델가져와야함
 
-    const user = new User(req.body)//req.body에 json형식으로 데이터있음. 있을 수 있는 것은 bodyparser덕분
+
+    const user = new User(req.body)
 
     try{
         await user.save();
@@ -46,17 +42,7 @@ app.post('/register', async(req,res) => {
         return res.json({success:false, err})
     }
     
-/*
-    user.save((err) => {//mongodb 메서드로
-        if(err) return res.json({success: false}, err)
-        return res.status(200).json(
-                                {success: true})
-    })
-    //if 저장시 에러가 있다면 json형식으로 client에게 성공실패 응답 & err메시지
-    //if 성공시 200(성공의 의미)
-    */
+
 })
 
 app.listen(port, () => console.log('Example app listening on port안뇽 ${port}!'))
-//5000번 에서 이 앱을 실행하는 것임.
-//의 의미: app이 port(3000)에 listen을 하면 콘솔이 프린트 되는 것임S
--- model/User.js
+++ model/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;//10자리인 salt를 이용해서 암호화
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -32,6 +33,28 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 });
+
+userSchema.pre('save', function(next){
+    //비밀번호를 암호화 시킨다.
+    var user = this;//위의 스키마 가리키
+    
+    if(user.isModified('password')){//닉네임변경시엔 재암호화되지 않도록, 비번변경시에만 재암호화
+
+    
+    bcrypt.genSalt(saltRounds, function(err,salt){
+        if(err) return next(err)
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err)
+            user.password = hash
+            next()
+        })
+    });
+    }
+})
+//mongoose 메소드
+//유저 모델에 유저 정보 저장 전에 function을 할 것이다.
+//=> 이 작업이 끝나고 index.js에서 모델 save(즉 이것을 next function으로 선언)
 
 const User = mongoose.model('User',userSchema)//(모델의이름,스키마이름)
 module.exports = {User} //이 모델을 다른데에서도 사용 가능하게
