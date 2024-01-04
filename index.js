114c2f59fac42976ec77cd2466fee4d0a7cbaf8d
Jo yerin <yerin65@naver.com>
2024-01-04 23:57:07 +0900

    jwt생성 및 쿠키저장

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require('express')
 const app = express()//새로운 express앱만들기
 const port = 9000
 
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const {User} = require("./model/User");
 
 
-
-
 app.use(bodyParser.urlencoded({extended:true}))
 //application/x-www-form-urlencoded된 데이터를 분석할 수 있게
-
 app.use(bodyParser.json());
 //aaplication/json타입으로 된 것을 분석할 수 있게
-
-
+app.use(cookieParser());
 
 
 const mongoose= require('mongoose')
@@ -34,7 +31,7 @@ app.post('/register', async(req,res) => {
     const user = new User(req.body)
 
     try{
-        await user.save();
+        await user.save();//save전 암호화 필요
         return res.status(200).json(
             {success: true})
         }
@@ -44,5 +41,37 @@ app.post('/register', async(req,res) => {
     
 
 })
+app.post('/login', async(req,res) => {
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다. mongo 메서드
+    User.findOne({email:req.body.email})
+    .then((user) => {
+        if(!user){
+            return res.json({
+                loginSucess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            });
+        }else
+        user.comparePassword(req.body.password , (err, isMatch) => {
+            if(!isMatch)
+                return res.json({loginSucess: false, message: "비밀번호가없어여"})
+            else //비밀번호까지 맞다면 토큰을 생성하기.
+            user.generateToken((err, user) => {//400은 err
+                if(err) return res.status(400).send(err);
 
-app.listen(port, () => console.log('Example app listening on port안뇽 ${port}!'))
+                // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지, 세션
+                res.cookie("amu_name_forCookie", user.token)
+                .status(200)
+                .json({loginSucess: true, userId: user._id})
+            })
+        })
+    })
+    .catch((err)=> {
+        return res.status(400).send(err);
+    })
+})
+        
+  
+
+
+
+app.listen(port, () => console.log('Example app listening on port안뇽 $`{port}`!'))
--- model/User.js
+++ model/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;//10자리인 salt를 이용해서 암호화
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -14,7 +16,7 @@ const userSchema = mongoose.Schema({
     },
     password:{
         type:String,
-        minlength: 5
+        minlength: 5,
     },
     lastname:{
         type:String,
@@ -34,7 +36,9 @@ const userSchema = mongoose.Schema({
     }
 });
 
-userSchema.pre('save', function(next){
+
+
+userSchema.pre('save', function(next){//save 메서드 실행되기 전
     //비밀번호를 암호화 시킨다.
     var user = this;//위의 스키마 가리키
     
@@ -42,7 +46,7 @@ userSchema.pre('save', function(next){
 
     
     bcrypt.genSalt(saltRounds, function(err,salt){
-        if(err) return next(err)
+        if(err) return next(err);
 
         bcrypt.hash(user.password, salt, function(err, hash){
             if(err) return next(err)
@@ -50,11 +54,64 @@ userSchema.pre('save', function(next){
             next()
         })
     });
+    } else{//비번이 아니라 다른 정보 변경시
+        next()
     }
 })
 //mongoose 메소드
 //유저 모델에 유저 정보 저장 전에 function을 할 것이다.
 //=> 이 작업이 끝나고 index.js에서 모델 save(즉 이것을 next function으로 선언)
+
+
+
+
+userSchema.methods.comparePassword = function(plainPassword, cb){//cb는 콜백함수
+
+    //plainPassword 12345 암호화된 비번$2b$10$H3ebswqZHjzg8RPERhTJd.qrkU3/Bv7GE1PUvxgaVVU3pFJpBWi0K"
+    //암호화된 비번을 복호화 불가능. -> plain을 암호화해서 비교해야함
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+        if(err) return cb(err);
+        else cb(null, isMatch)//에러가 없다와 true
+    })
+}
+userSchema.methods.generateToken = async function(cb){//콜백변수 하나
+
+    var user = this//es5문법
+
+    //jsonwebtoken을 이용해서 token을 생성하기
+    //user._id + this~를 토큰에 넣어주고, 토큰에서 this~로 user._id판별 => 'this~'도 기억해야함
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token;
+
+
+    async function saveUser(user) {
+        try {
+          await user.save();
+          const savedUser = await User.findById(user);
+          return savedUser;
+        } catch (err) {
+          throw err;
+        }
+      }
+      
+      try {
+        const savedUser = await saveUser(user);
+        cb(null, savedUser);
+      } catch (err) {
+        cb(err);
+      }
+    
+    /*
+    user.token = token
+    
+    user.save(function (err, user){
+        if(err) cb(err)
+        else cb(null, user)
+    })
+    */
+
+}
+
 
 const User = mongoose.model('User',userSchema)//(모델의이름,스키마이름)
 module.exports = {User} //이 모델을 다른데에서도 사용 가능하게
