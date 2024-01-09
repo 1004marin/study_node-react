10d488b576abb1bb8e82fd56299c0a6a6238387f
Jo yerin <yerin65@naver.com>
2024-01-09 23:29:07 +0900

    reducer,action이용 로그인 구현

---
+++ client/src/_reducers/user_reducer.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import {LOGIN_USER} from '../_actions/types';
+
+//prevState + action을 가졌으니, nextState 돌려주기
+export default function (state={}, action) {
+    switch (action.type) {//why 스위치문법? action의 type이Login_user만은 아니니까 타입마다 다른 조치 취하기
+        case LOGIN_USER:
+            return {...state}//위를 그대로 가져옴. 빈상태
+            break;
+    
+        default:
+            break;
+    }
+}
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,22 +1,46 @@
-import React,{useState} from 'react'
+import { response } from 'express';
+import React,{useState} from 'react';
+import Axios from 'axios';
+import {useDispatch} from 'react-redux';
 
 function LoginPage() {
+    const dispatch = useDispatch()
     const [Email, setEmail] = useState("")
     const [Password,setPassword] = useState("")
 
+    const onEmailHandler = (e) =>{
+        setEmail(e.currentTarget.value)
+    }
+    const onPasswordHandler = (e) => {
+       setPassword(e.currentTarget.value)
+    }
 
-  return (
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault()//없으면 login클릭시 refresh됨. 원래 해야할 일들을 하지 못하고 새고해버림!
+        
+        let body = {
+            email: Email,
+            Password: Password
+        }
+        dispatch(loginUser(body))//loginUser라는 action
+
+    }
+
+
+  return (//form과 button에 모두 submit주는 이유는, and design때매!
     <div style={{height:'100vh', width:'100%', backgroundColor:"pink",
-    display:'flex', justifyContent:'center', alignItems:'center'}}>
+    display:'flex', justifyContent:'center', alignItems:'center'}}
+        onSubmit={onSubmitHandler}>
         
         <form style={{display:"flex", flexDirection:"column"}}>
             <label>Email</label>
-            <input type="email" value={Email} onChange/>
+            <input type="email" value={Email} onChange={onEmailHandler}/>
             <label>Password</label>
-            <input type="password" value={Password} onChange/>
+            <input type="password" value={Password} onChange={onPasswordHandler}/>
 
             <br/>
-            <button>
+            <button type="submit">
                 login
             </button>
         </form>   
